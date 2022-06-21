1b5743807f5130643f2843b4a2c6ef6db0a5b124
«Kamila» <koskanova@outlook.com>
2022-06-21 14:59:02 +0300

    Added the Homework 1 solution

--- Seminar1/Homework1.py
+++ Seminar1/Homework1.py
@@ -1,5 +1,5 @@
 # Task 1:
-# Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+# Напишите программу, которая принимает на вход цифру, обозначающую день недели,
 # и проверяет, является ли этот день выходным.
 
 # Пример:
@@ -7,9 +7,19 @@
 # 7 -> да
 # 1 -> нет
 
+# weekday = int(input("Enter the number of the day of the week: "))
+
+# if weekday == 6 or weekday == 7:
+#     print("Hooray, this is a weekend.")
+# elif 0 < weekday < 6:
+#     print("Ohh, this is not a weekend.")
+# else:
+#     print("Invalid number. Enter the number from 1 to 7. ")
+
+
 # Task 2:
-# Напишите программу, которая принимает на вход координаты точки (X и Y), 
-# и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой 
+# Напишите программу, которая принимает на вход координаты точки (X и Y),
+# и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой
 # оси она находится).
 
 # Пример:
@@ -17,14 +27,35 @@
 # x=2; y=4-> 1
 # x=-34; y=-30 -> 3
 
-# Task 3:
-# Напишите программу, которая по заданному номеру четверти, показывает диапазон 
-# возможных координат точек в этой четверти (x и y).
+# x = int(input("Enter the x coordinate: "))
+# y = int(input("Enter the y coordinate: "))
+
+# if x > 0 and y > 0:
+#     print(f'x coordinate = {x} and y coordinate = {y} are in the first quarter.')
+# elif x < 0 and y > 0:
+#     print(f'x coordinate = {x} and y coordinate = {y} are in the second quarter.')
+# elif x < 0 and y < 0:
+#     print(f'x coordinate = {x} and y coordinate = {y} are in the third quarter.')
+# elif x > 0 and y < 0:
+#     print(f'x coordinate = {x} and y coordinate = {y} are in the fourth quarter.')
+# else:
+#     print("Invalid number. Your coordinates cannot be equal to zero. Try again.")
+
 
 # Task 4
-# Напишите программу, которая принимает на вход координаты двух точек и находит 
+# Напишите программу, которая принимает на вход координаты двух точек и находит
 # расстояние между ними в 2D пространстве.
 
 # Пример:
 # A (3,6); B (2,1) -> 5,09
 # A (7,-5); B (1,-1) -> 7,21
+
+# from math import sqrt
+
+# x_A = int(input("Enter the X coordinate of point A: "))
+# y_A = int(input("Enter the Y coordinate of point A: "))
+# x_B = int(input("Enter the X coordinate of point B: "))
+# y_B = int(input("Enter the Y coordinate of point B: "))
+
+# distance = sqrt((x_B - x_A)**2 + (y_B - y_A)**2)
+# print(f"The distance between point A({x_A};{y_A}) and point B({x_B};{y_B}) is {round(distance,2)}.")
